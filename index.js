aa9e1b7d1e9865275e8b37e9f447cf4422dcc08f
nazimz99 <53349978+nazimz99@users.noreply.github.com>
2020-05-01 17:27:07 -0700

    Added coursessql to routes

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const pool = new Pool({
 
 app.use('/hello', require('./routes/hello.js'));
 app.use('/params', require('./routes/params.js'))
+app.use('/', require('./routes/coursessql.js'));
 /*app.get("/hello", (req, res)=>{
     res.send({
         message: "Hello, you send a GET request"
---
+++ routes/coursessql.js
@@ -0,0 +1,68 @@
+const express = require('express');
+
+//pg-promise is a postgres library that uses javascript promises
+const pg = require('pg-promise');
+//We have to set ssl usage to true for Heroku to accept our connection
+pg.pg.defaults.ssl = true;
+
+//Create connection to Heroku Database
+let db = pg(process.env.DATABASE_URL);
+
+if (!db) {
+    console.log("Make sure that postgres is an addon!");
+    process.exit();
+}
+
+let router = express.Router();
+
+const bodyparser = require("body-parser");
+//This allows parsing of the body of POST requests that are encoded in JSON
+router.use(bodyParser.json());
+
+router.post("/addcourse", (req, res) => {
+    // Parameters for the courses
+    let id = req.body['id'];
+    let shortdesc = req.body['shortdesc'];
+    let longdesc = req.body['longdesc'];
+    let prereqs = req.body['prereqs'];
+
+    if (id && shortdesc && longdesc && prereqs) {
+        db.none("INSERT INTO courses VALUES ($1, $2, $3, $4)", [id, shortdesc, longdesc, prereqs])
+            .then(() => {
+                //We successfully added the course, let the user know
+                res.send({
+                    success: true
+                });
+            }).catch((err) => {
+            //log the error
+            console.log(err);
+            res.send({
+                success: false,
+                error: err
+            });
+        });
+    } else {
+        res.send({
+            success: false,
+            input: req.body,
+            error: "Missing required information"
+        });
+    }
+});
+
+router.get("/courses", (req, res) => {
+    db.manyOrNone('SELECT * FROM courses')
+    //If successful, run function passed into .then()
+        .then((data) => {
+            res.send({
+                success: true,
+                names: data
+            });
+        }).catch((error) => {
+            console.log(error);
+            res.send({
+                success: false,
+                error: error
+            });
+        });
+});
