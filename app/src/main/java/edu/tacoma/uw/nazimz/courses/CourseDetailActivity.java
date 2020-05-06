890d5a451e2916d3d364d9b3909cd17ae0014bde
nazimz99 <53349978+nazimz99@users.noreply.github.com>
2020-05-05 19:42:27 -0700

    Add files via upload
    CourseDetailActivity added, didn't go through before.

---
+++ app/src/main/java/edu/tacoma/uw/nazimz/courses/CourseDetailActivity.java
@@ -0,0 +1,180 @@
+package edu.tacoma.uw.nazimz.courses;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import model.Course;
+
+/**
+ * An activity representing a single Item detail screen. This
+ * activity is only used on narrow width devices. On tablet-size devices,
+ * item details are presented side-by-side with a list of items
+ * in a {@link CourseListActivity}.
+ */
+public class CourseDetailActivity extends AppCompatActivity
+    implements CourseAddFragment.AddListener {
+
+    public static final String ADD_COURSE = "ADD_COURSE";
+    private JSONObject mCourseJSON;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // savedInstanceState is non-null when there is fragment state
+        // saved from previous configurations of this activity
+        // (e.g. when rotating the screen from portrait to landscape).
+        // In this case, the fragment will automatically be re-added
+        // to its container so we don't need to manually add it.
+        // For more information, see the Fragments API guide at:
+        //
+        // http://developer.android.com/guide/components/fragments.html
+        //
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+            Bundle arguments = new Bundle();
+            if (getIntent().getSerializableExtra(CourseDetailFragment.ARG_ITEM_ID) != null) {
+                arguments.putSerializable(CourseDetailFragment.ARG_ITEM_ID,
+                        getIntent().getSerializableExtra(CourseDetailFragment.ARG_ITEM_ID));
+                CourseDetailFragment fragment = new CourseDetailFragment();
+                fragment.setArguments(arguments);
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.item_detail_container, fragment)
+                        .commit();
+            }
+            else if (getIntent().getBooleanExtra(CourseDetailActivity.ADD_COURSE, false)) {
+                CourseAddFragment fragment = new CourseAddFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.item_detail_container, fragment)
+                        .commit();
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            navigateUpTo(new Intent(this, CourseListActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void addCourse(Course course) {
+        StringBuilder url = new StringBuilder(getString(R.string.add_course));
+
+        //Construct a JSONObject to build a formatted message to send.
+        mCourseJSON = new JSONObject();
+        try {
+            mCourseJSON.put(Course.ID, course.getCourseID());
+            mCourseJSON.put(Course.SHORT_DESC, course.getCourseShortDesc());
+            mCourseJSON.put(Course.LONG_DESC, course.getCourseLongDesc());
+            mCourseJSON.put(Course.PRE_REQS, course.getCoursePrereqs());
+        } catch (JSONException e) {
+            Toast.makeText(this, "Error with JSON creation on adding a course: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class AddCourseAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.setDoOutput(true);
+                    OutputStreamWriter wr =
+                            new OutputStreamWriter(urlConnection.getOutputStream());
+
+                    // For Debugging
+                    Log.i(ADD_COURSE, mCourseJSON.toString());
+                    wr.write(mCourseJSON.toString());
+                    wr.flush();
+                    wr.close();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to add the new course, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.startsWith("Unable to add the new course")) {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                if (jsonObject.getBoolean("success")) {
+                    Toast.makeText(getApplicationContext(), "Course Added successfully"
+                            , Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Course couldn't be added: "
+                                    + jsonObject.getString("error")
+                            , Toast.LENGTH_LONG).show();
+                    Log.e(ADD_COURSE, jsonObject.getString("error"));
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "JSON Parsing error on Adding course"
+                                + e.getMessage()
+                        , Toast.LENGTH_LONG).show();
+                Log.e(ADD_COURSE, e.getMessage());
+            }
+        }
+    }
+}
